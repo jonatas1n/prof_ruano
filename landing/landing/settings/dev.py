baf53f91ba0a9bfefb3c51705ce4b68899ba51fe
jonatas1n <jonatasgeek@gmail.com>
2024-09-27 20:40:19 -0300

    update dev file

--- landing/landing/settings/dev.py
+++ landing/landing/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = bool(os.environ.get('DEBUG', True))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-jp%lrm22_91)n%$wxr^b=*^zq=09v%)z@6!=j0+v0r$^ckj(va"
