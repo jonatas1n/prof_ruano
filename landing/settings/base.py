6aab015becc616f0f7c4916e8e43e28be31aad62
jonatas1n <jonatasgeek@gmail.com>
2024-11-12 21:53:46 -0300

    update

--- landing/settings/base.py
+++ landing/settings/base.py
@@ -101,11 +101,18 @@ WSGI_APPLICATION = "landing.wsgi.application"
 
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#     )
+# }
 
 # DATABASES = {
 #     "default": {
