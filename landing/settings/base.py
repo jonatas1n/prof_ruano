9c715af124dc698a1f7ac57d16dddc3c0f1725c6
jonatas1n <jonatasgeek@gmail.com>
2024-11-12 23:03:58 -0300

    update db

--- landing/settings/base.py
+++ landing/settings/base.py
@@ -103,14 +103,14 @@ WSGI_APPLICATION = "landing.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
-            'sslmode': os.getenv('DB_SSLMODE'),
+            'sslmode': os.getenv('DB_SSLMODE', 'require'),
         },
     }
 }
