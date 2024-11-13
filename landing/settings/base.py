d2c4ea813739d1959e48caf129387767e7709dae
jonatas1n <jonatasgeek@gmail.com>
2024-11-12 22:27:06 -0300

    update database

--- landing/settings/base.py
+++ landing/settings/base.py
@@ -107,12 +107,12 @@ WSGI_APPLICATION = "landing.wsgi.application"
 #     }
 # }
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#     )
+# }
 
 DATABASES = {
     'default': {
